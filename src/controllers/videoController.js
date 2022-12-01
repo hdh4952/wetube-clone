1dda2da948dd370ecad29a11fc19f6b54066db84
hdh4952 <gkseogml4959@gmail.com>
2022-12-01 11:27:00 +0900

    Feat: add Pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,36 @@
-const trending = (req, res) => res.send("Home Page Videos");
-const search = (req, res) => res.send("Search Video");
-const see = (req, res) => {
-  return res.send(`Watch Video ${req.params.id}`);
+const trending = (req, res) => {
+  const videos = [
+    {
+      title: "Video #1",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Video #2",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 2,
+    },
+    {
+      title: "Video #3",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 3,
+    },
+  ];
+  return res.render("home", { pageTitle: "Home", videos });
 };
-const edit = (req, res) => res.send("Edit Video");
-const remove = (req, res) => res.send("Remove Video");
+const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
+const search = (req, res) => res.send("Search Video");
 const upload = (req, res) => res.send("Upload Video");
+const remove = (req, res) => res.send("Remove Video");
 
 export { trending, see, edit, search, remove, upload };
