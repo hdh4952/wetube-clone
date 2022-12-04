c6db709b6700219e62b952d1c3b4f89b09189098
hdh4952 <gkseogml4959@gmail.com>
2022-12-04 16:07:29 +0900

    Feat: MongoDB, mongoose
    install MongoDB and mongoose
    add video CRUD

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,36 +1,90 @@
-const trending = (req, res) => {
-  const videos = [
-    {
-      title: "Video #1",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "Video #2",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 2,
-    },
-    {
-      title: "Video #3",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 3,
-    },
-  ];
+import Video from "../models/Video";
+
+const home = async (req, res) => {
+  const videos = await Video.find({}).sort({ title: "asc" });
   return res.render("home", { pageTitle: "Home", videos });
 };
-const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
-const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
-const search = (req, res) => res.send("Search Video");
-const upload = (req, res) => res.send("Upload Video");
-const remove = (req, res) => res.send("Remove Video");
 
-export { trending, see, edit, search, remove, upload };
+const watch = async (req, res) => {
+  const { id } = req.params;
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found" });
+  }
+  return res.render("watch", { pageTitle: video.title, video });
+};
+
+const getEdit = async (req, res) => {
+  const { id } = req.params;
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found" });
+  }
+  return res.render("edit", { pageTitle: `Edit ${video.title}`, video });
+};
+
+const postEdit = async (req, res) => {
+  const { id } = req.params;
+  const { title, description, hashtags } = req.body;
+  const video = await Video.exists({ _id: id });
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found" });
+  }
+  await Video.findByIdAndUpdate(id, {
+    title,
+    description,
+    hashtags: Video.formatHashtags(hashtags),
+  });
+  return res.redirect(`/videos/${id}`);
+};
+
+const getUpload = (req, res) => {
+  return res.render("upload", { pageTitle: "Upload Video" });
+};
+
+const postUpload = async (req, res) => {
+  const { title, description, hashtags } = req.body;
+  try {
+    await Video.create({
+      title,
+      description,
+      hashtags: Video.formatHashtags(hashtags),
+    });
+    return res.redirect("/");
+  } catch (error) {
+    return res.render("upload", {
+      pageTitle: "Upload Video",
+      errorMessage: error._message,
+    });
+  }
+};
+
+const deleteVideo = async (req, res) => {
+  const { id } = req.params;
+  await Video.findByIdAndDelete(id);
+  return res.redirect("/");
+};
+
+const search = async (req, res) => {
+  const { keyword } = req.query;
+  let videos = [];
+  if (keyword) {
+    videos = await Video.find({
+      title: {
+        $regex: new RegExp(keyword, "i"),
+      },
+    });
+  }
+  return res.render("search", { pageTitle: "Search", videos });
+};
+
+export {
+  home,
+  watch,
+  getEdit,
+  postEdit,
+  getUpload,
+  postUpload,
+  deleteVideo,
+  search,
+};
--- src/server.js
+++ src/server.js
@@ -4,17 +4,14 @@ import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 
-const PORT = 4000;
-
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
 app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
 
-app.listen(PORT, () =>
-  console.log(`✅Server listening on port http://localhost:${PORT}`)
-);
+export default app;
